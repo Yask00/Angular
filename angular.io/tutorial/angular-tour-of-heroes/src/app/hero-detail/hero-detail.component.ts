70329acf819b0c366c7fab1864a7e63071a12b0d
Yasen Trayanov <jasstreb@abv.bg>
2019-05-25 18:04:37 +0300

    step 6 - adding http requests through simulated api with httpClient

--- angular.io/tutorial/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular.io/tutorial/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { HeroService }  from '../hero.service';
+import { HeroService } from '../hero.service';
 
 @Component({
 	selector: 'app-hero-detail',
@@ -25,7 +25,12 @@ export class HeroDetailComponent implements OnInit {
 	getHero(): void {
 		const id = +this.route.snapshot.paramMap.get('id');
 		this.heroService.getHero(id)
-		  .subscribe(hero => this.hero = hero);
+			.subscribe(hero => this.hero = hero);
+	}
+
+	save(): void {
+		this.heroService.updateHero(this.hero)
+			.subscribe(() => this.goBack());
 	}
 
 	goBack(): void {
